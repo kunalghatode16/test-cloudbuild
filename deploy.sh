f7284dd23d2f4c00ff1101209f23207fde087cc2
kunalghatode16 <158257966+kunalghatode16@users.noreply.github.com>
2024-09-20 16:12:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
 
-# Deploy function1
-gcloud functions deploy testing-push \
-  --runtime python39 \
-  --trigger-http \
-  --entry-point=main \
-  --allow-unauthenticated \
-  --source=function1 \
-  --region=us-central1
+#!/bin/bash
+# Check which folder is being deployed
+if [ "$1" == "." ]; then
+  gcloud functions deploy function-1 \
+    --runtime python311 \
+    --trigger-http \
+    --source= .
+    --region=us-central1
+elif [ "$1" == "cicd" ]; then
+  gcloud functions deploy function-2 \
+    --runtime python311 \
+    --trigger-http \
+    --source=cicd \
+    --region=us-central1
+elif [ "$1" == "cide-folder1" ]; then
+  gcloud functions deploy function-3 \
+    --runtime python311 \
+    --trigger-http \
+    --source=cicd-folder1 \
+    --region=us-central1
+else
+  echo "Unknown function. Exiting."
+  exit 1
+fi
 
